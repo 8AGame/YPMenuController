60aa84d0a4a3219fbc27846c7ea7f7f11f8ce8cb
Yaping Liu <lypJTD@163.com>
2019-03-20 16:30:46 +0800

    inital

---
+++ YPMenuController/Classes/Utility/YPMenuItem+Private.h
@@ -0,0 +1,18 @@
+//
+//  YPMenuItem+Private.h
+//  Pods-YPMenuController_Example
+//
+//  Created by Yaping Liu on 3/20/19.
+//
+
+#import "YPMenuItem.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YPMenuItem (Private)
+
+@property (nonatomic, strong) UIButton *menuButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
